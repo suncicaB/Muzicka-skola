37e602bfe8cfc244cf811f7d470ca8d974cd6908
jelaB <jelababic95@gmail.com>
2020-05-22 23:54:14 +0200

    Sklonjen stari kod iz main-a

--- main.cpp
+++ main.cpp
@@ -54,15 +54,6 @@ int main(){
 	}
 
 
-    Osoba o1, o2("Pera", "Peric", 10);
-    o2.ispisiOsoba();
-
-    Ucenik u1,u2("Suncica", "Babic", 16, RazredState::drugi);
-    u1.citajFajl("ucenici.txt");
-    u2.upisiUcenika(u2, 100);
-
-    Profesor p1;
-    p1.citajFajl("profesori.txt");
     return 0;
 }
 
